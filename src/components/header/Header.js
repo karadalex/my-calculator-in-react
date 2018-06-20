7de1b882f68f56f5059b066848d98d171cd354ce
Alex Karadimos <karadalex@gmail.com>
2018-06-20 22:07:01 +0300

    removed react logo

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './Header.css';
 
 class Header extends Component {
   render() {
     return (
         <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
             <h1 className="App-title">My Calculator in React</h1>
         </header>
     );
