41aaffe831ecf1894bfde13537a75562b3ab1d61
Alex Karadimos <karadalex@gmail.com>
2018-06-20 22:52:02 +0300

    simple operations, but with bugs

--- src/components/calculator/Calculator.js
+++ src/components/calculator/Calculator.js
@@ -4,17 +4,43 @@ import CalculatorScreen from '../calculator-screen/CalculatorScreen';
 import CalculatorButtons from '../calculator-buttons/CalculatorButtons';
 import Grid from '@material-ui/core/Grid';
 
+
 class Calculator extends Component {
   constructor(props) {
     super(props);
     this.state = {
       buttonClicked: 0,
-      result: 0
+      result: 0,
+      latestOperator: ""
     }
   }
 
-  handleButtonClick(i) {
-    this.setState({result: i});
+  handleButtonClick(x) {
+    if (Number.isInteger(x)) {
+      var newResult = this.calculate(x)
+      this.setState({result: newResult});
+    } else {
+        this.setState({latestOperator: x});
+    }
+  }
+
+  calculate(num) {
+    var op = this.state.latestOperator;
+    var prev = this.state.result;
+    switch (op) {
+      case "+":
+        return prev + num;
+      case "-":
+        return prev - num;
+      case "x":
+        return prev * num;
+      case "/":
+        if (num != 0) {
+          return prev / num;
+        } else {
+          return NaN;
+        }
+    }
   }
 
   render() {
@@ -25,7 +51,7 @@ class Calculator extends Component {
                 <CalculatorScreen result={this.state.result}/>
               </Grid>
               <Grid item xs={12}>
-                <CalculatorButtons onClick={(i) => this.handleButtonClick(i)}/>
+                <CalculatorButtons onClick={(x) => this.handleButtonClick(x)}/>
               </Grid>
             </Grid>
         </div>
